94ddeb8dbc5429a92a3965fb88c8d6a0a1b8f7b6
mbartnik <michal.bartnik@nokia.com>
2016-10-26 16:55:03 +0200

    Add session file for Notepad++ - 'walidacja' directory

---
+++ walidacja/npp_session.js
@@ -0,0 +1,10 @@
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="2">
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="5137" startPos="0" endPos="0" selMode="0" lang="CSS" encoding="-1" filename="D:\userdata\mbartnik\My Documents\Private\gitHub\learning\walidacja\css\style.css" backupFilePath="" originalFileLastModifTimestamp="1477482435" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="5137" startPos="0" endPos="0" selMode="0" lang="HTML" encoding="-1" filename="D:\userdata\mbartnik\My Documents\Private\gitHub\learning\walidacja\index.html" backupFilePath="" originalFileLastModifTimestamp="1477482435" />
+            <File firstVisibleLine="24" xOffset="0" scrollWidth="5137" startPos="680" endPos="680" selMode="0" lang="JavaScript" encoding="-1" filename="D:\userdata\mbartnik\My Documents\Private\gitHub\learning\walidacja\js\site.js" backupFilePath="" originalFileLastModifTimestamp="1477482435" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
